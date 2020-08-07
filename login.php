11c449e21662a7011ee41c3c699224da592da3ca
CoolHackerBoy <65199906+CoolHackerBoy@users.noreply.github.com>
2020-08-07 16:20:55 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<form method="post" action="login_process.php">
+	<h2>Login</h2>
+	<input placeholder="username" type="text" name="username">
+	<br>
+	<input placeholder="password" type="password" name="password">
+	<br>
+	<input type="submit" value="Log In">
+</form>
+<hr>
+<form method="post" action="register_process.php">
+	<h2>Create an Account</h2>
+	<input placeholder="username" type="text" name="new_user">
+	<br>
+	<input placeholder="password" type="password" name="new_pw">
+	<br>
+	<input type="submit" value="Register">
+</form>
