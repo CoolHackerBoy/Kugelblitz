20f702dcccd052c164f8bc7971c237a376ffe3cb
CoolHackerBoy <65199906+CoolHackerBoy@users.noreply.github.com>
2020-08-07 16:44:14 -0700

    Update update_position.php

--- update_position.php
+++ update_position.php
@@ -1,8 +1,8 @@
 <?php
 
 	session_start();
-	require_once('../../show_php_errors.php');
-	require_once('../../db_con.php');
+	require_once('show_php_errors.php');
+	require_once('db_con.php');
 	
 	$db_con = mysqli_connect($db_host, $db_user, $db_pass, 'cc4k__hackerboy');
 
