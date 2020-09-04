cf7725cb125690e9b03370eb08ca4e945cab88a0
yoy333 <colinjdoherty@outlook.com>
2020-09-04 17:43:16 -0600

    added auto-reload

--- js_vars.php
+++ js_vars.php
@@ -1,4 +1,5 @@
 <script>
 	var x_pos = <?php echo $_SESSION['x_pos']; ?>;
 	var y_pos = <?php echo $_SESSION['y_pos']; ?>;
+	var username = <?php echo '\''; echo $_SESSION['username']; echo '\''?>
 </script>
