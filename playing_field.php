997efc6231124687595e5fa4358359353d02cd2c
yoy333 <colinjdoherty@outlook.com>
2020-09-25 16:53:57 -0600

    ZERO

--- playing_field.php
+++ playing_field.php
@@ -8,12 +8,15 @@
 	
 	function rollTurn(){
 		include('../db_con.php');
-		$roll = rand(1,1000)/10;
+		$roll = rand(1,100000)/10;
 		$db_con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 		$stmt = 'UPDATE kb_users SET roll='.$roll.' WHERE username="'.$_SESSION['username'].'"';
 		if(mysqli_query($db_con, $stmt)){
 			echo 'yes';
 		}
+		setTimeout(function(){
+			
+		}, )
 	}
 	
 	rollTurn();
